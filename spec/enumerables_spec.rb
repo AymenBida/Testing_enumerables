0f34053b62af3f5ddf20b8a53cc6c65962a1b761
ZawadiSumba66 <rosesumba98@gmail.com>
2021-01-20 14:33:52 +0300

    add test for #my_all? method

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -74,4 +74,62 @@ describe Enumerable do
       end
     end
   end
+
+  describe '#my_all?' do
+    let(:arrs) {[1, 2, 't']}
+    context 'when given an array and a block containing a condition ' do
+      it 'should return true if every element in the array meets the condition' do
+        expect(arr.my_all? {|a| a.is_a?(String)}).to be true 
+      end
+      
+      it 'should return false if one of the elements does not meet the condition' do
+        expect(arrs.my_all? {|a| a.is_a?(Integer)}).to be false
+      end
+    end
+
+    context 'when given no block' do
+      let (:falsy_array) {[false, nil, false, nil]}
+      it 'should return true if each element in the block is truthy' do
+        expect(arr.my_all?).to be true
+      end
+
+      it 'should return false if each element in the block is falsy' do
+        expect(falsy_array.my_all?).to be false
+      end
+    end
+
+    context 'when given an argument without a block' do
+      let (:same_array) {['a','a','a']}
+      let (:diff_array) {['a','a','b']}
+      it 'should return true if every element is equal to the argument' do
+        expect(same_array.my_all?('a')).to be true
+      end
+    
+      it 'should return false if one or more elements is not equal to the argument' do
+        expect(diff_array.my_all?('a')).to be false
+      end
+    end
+
+    context 'when given a class as an argument' do
+      it 'should return true if every element is included in the class' do
+        expect(arr.my_all?(String)).to  be true
+      end
+
+      it 'should return false if one or more elements is not included in the class' do
+        expect(arrs.my_all?(String)).to  be false
+      end
+    end
+
+    context 'when given a Regexp as an argument' do
+      let (:reg_array) {[/d/,/g/]}
+      let (:anti_reg) {[/d/,7]}
+      it 'should return true if every element is included in the reqexp' do
+        expect(reg_array.my_all?(Regexp)).to  be true
+      end
+
+      it 'should return false if one or more elements is not included in the regexp' do
+        expect(anti_reg.my_all?(Regexp)).to  be false
+      end
+    end
+  end
 end
