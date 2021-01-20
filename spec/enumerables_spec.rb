95c4d9e519da2096d0529c4cd18339fedf1e939d
AymenBida <aymen.bida.00@gmail.com>
2021-01-20 10:46:54 +0100

    Add tests for #my_each method

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,32 @@
+require_relative '../bin/enumerables.rb'
+
+describe Enumerable do
+  let(:arr) { ['a', 'b', 'c'] }
+  let(:range) { (1..5) }
+  let(:hash) { {a: '1', b: '2'} }
+  describe '#my_each' do
+    context 'when given an array as an argument with a block' do
+      it 'should return the same array' do
+        expect(arr.my_each { |a| a }).to match_array(['a', 'b', 'c'])
+      end
+    end
+
+    context 'when given a range as an argument with a block' do
+      it 'should return an array that matches the range numbers' do
+        expect(range.my_each { |a| a }).to match_array([1, 2, 3, 4, 5])
+      end
+    end
+
+    context 'when given a hash as an argument with a block' do
+      it 'should return an array that matches the hash' do
+        expect(hash.my_each { |a| a }).to match_array([[:a, '1'], [:b, '2']]) 
+      end
+    end
+
+    context 'when given no block' do
+      it 'should return an enumerator' do
+        expect(arr.my_each).to be_an(Enumerator)
+      end
+    end
+  end
+end
